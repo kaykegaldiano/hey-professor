ee1f2694100587b7f72eb5a822ee802ab0cdd47f
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-02-12 21:17:30 -0300

    hey-12: testing if should not be able to like a question twice

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -26,3 +26,21 @@ it('shoud be able to like a question', function () {
         'unlike' => 0,
     ]);
 });
+
+it('should not be able to like a question twice', function () {
+    // Arrange
+    $user = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    // Act
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    // Assert
+    expect($user->votes()->where('question_id', $question->id)->get())
+        ->toHaveCount(1);
+});
