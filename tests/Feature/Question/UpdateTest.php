ebf1e050ebca547dc220f4df967621f82cc2978d
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-02-20 05:28:38 -0300

    hey-16: should make sure that only questions with status DRAFT can be updated and only if the question was created by the logged user

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -23,3 +23,26 @@ it('should update the question in the database', function () {
         ->question
         ->toBe('Was the question updated?');
 });
+
+it('should make sure that only questions with status DRAFT can be updated', function () {
+    $user = User::factory()->create();
+    $notDraftQuestion = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $notDraftQuestion))->assertForbidden();
+    put(route('question.update', $draftQuestion), ['question' => 'Was the question updated?'])->assertRedirect();
+});
+
+it('makes sure only the person who has created the question can update it', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+    put(route('question.update', $question))->assertForbidden();
+
+    actingAs($rightUser);
+    put(route('question.update', $question), ['question' => 'Was the question updated?'])->assertRedirect();
+});
