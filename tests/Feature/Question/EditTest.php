4578a40a61a633de0076e97deeb602a4f63071e8
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-02-20 04:52:33 -0300

    hey-16: it makes sure only the person who has created the question can edit it

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -36,3 +36,15 @@ it('should make sure that only questions with status DRAFT can be edited', funct
     get(route('question.edit', $notDraftQuestion))->assertForbidden();
     get(route('question.edit', $draftQuestion))->assertSuccessful();
 });
+
+it('makes sure only the person who has created the question can edit it', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+    get(route('question.edit', $question))->assertForbidden();
+
+    actingAs($rightUser);
+    get(route('question.edit', $question))->assertSuccessful();
+});
